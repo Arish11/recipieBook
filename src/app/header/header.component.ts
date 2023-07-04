bf7c680348312a01dca8deaf470d49e6dfe1f3ed
arish11 <abhishekarya.8596@gmail.com>
2023-07-04 12:40:45 +0530

    executed routing using structural directives ngIf

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component ({
     selector:"app-header",
@@ -8,5 +8,22 @@ import { Component } from "@angular/core";
 })
 
 export class Header {
+    recipeFlag:boolean = false
+   // shoppingListFlag:boolean = false
+    
 
+    onRecipeClicked(val: string){
+      this.recipeFlag =  val=== "Recipes" ? true : false
+     // this.shoppingListFlag = false
+    }
+
+    
+    // onShoppingClicked(val: string){
+    //     this.shoppingListFlag =  val=== "Shopping List" ? true : false
+    //     this.recipeFlag = false
+    //   }
+
+    onShoppingClicked(){
+        this.recipeFlag = false
+    }
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -8,9 +8,9 @@ import { Recipe } from './recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-  recipes : Recipe[] = [ new Recipe("test","test","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
-  new Recipe("test","test","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
-  new Recipe("test","test","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg")]
+  recipes : Recipe[] = [ new Recipe("Panner","Masala","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
+  new Recipe("Chole","Punjabi","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
+  new Recipe("Soda","Mitha","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg")]
   
   constructor() { }
 

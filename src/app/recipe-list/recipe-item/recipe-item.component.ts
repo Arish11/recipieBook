f82fad9e0002f59507397262fded4e3d142ad8e3
arish11 <abhishekarya.8596@gmail.com>
2023-07-04 13:48:32 +0530

    displaying recItem on recDetails

--- src/app/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  @Input()  recipes : Recipe[] = [ new Recipe("","","")] 
+  @Input()rec!: Recipe;
   constructor() { }
-
+  @Output() recipeSelected = new EventEmitter<void>()
   ngOnInit(): void {
   }
-
+  onRecipeSelect(){
+    this.recipeSelected.emit()
+  }
 }
--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -11,10 +11,15 @@ export class RecipeListComponent implements OnInit {
   recipes : Recipe[] = [ new Recipe("Panner","Masala","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
   new Recipe("Chole","Punjabi","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg"),
   new Recipe("Soda","Mitha","https://upload.wikimedia.org/wikipedia/commons/3/39/Recipe.jpg")]
+  recipeToShow!: Recipe;
   
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onRecDetails(recDetail: any){
+    console.log(recDetail);
+    this.recipeToShow = recDetail
+  }
 }

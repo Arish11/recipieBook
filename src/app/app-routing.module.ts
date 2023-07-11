3e6440c117e399b0674564b69b6f56cbedc133a8
arish11 <abhishekarya.8596@gmail.com>
2023-07-11 18:49:40 +0530

    implemented basic routing for recipe and shopping List

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', redirectTo : '/recipes', pathMatch:'full'},
+  {path : 'recipes', component: RecipeListComponent},
+  {path: 'shoppingList', component: ShoppingListComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

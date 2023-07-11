652ce52c9db3d2b6bce154df519ccabb0b16cb0a
arish11 <abhishekarya.8596@gmail.com>
2023-07-12 00:10:35 +0530

    Programmatic selection of recipe item from recipe list

--- src/app/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,11 +10,20 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input()rec!: Recipe;
-  constructor(private _recServ:RecipeService) { }
+  constructor(private _recServ:RecipeService,
+              private router : Router) { }
   // @Output() recipeSelected = new EventEmitter<void>()
   ngOnInit(): void {
     
   }
+
+  //We can either navigate through recipe list by the below method on each click event or simply use [routerLink] for it
+
+  // onNavigate(){
+  //   this.router.navigate(['/recipes',this.rec.id])
+  // }
+
+
   // onRecipeSelect(){
   //   //this.recipeSelected.emit()
   //   this._recServ.selectRecipe(this.rec)

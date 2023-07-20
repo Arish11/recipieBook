4418eee842220f9069b33468b795c0008524bf42
arish11 <abhishekarya.8596@gmail.com>
2023-07-20 13:49:22 +0530

    using template driven form for adding items to shopping list

--- src/app/shopping-list/shop-list-edit/shop-list-edit.component.ts
+++ src/app/shopping-list/shop-list-edit/shop-list-edit.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import { Ingridient } from 'src/app/shared/ingridient.model';
 import { ShoppingListService } from '../shoppingList.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-shop-list-edit',
@@ -9,16 +10,14 @@ import { ShoppingListService } from '../shoppingList.service';
 })
 export class ShopListEditComponent implements OnInit {
 
-  
-  
-
   constructor(private _shopSrv:ShoppingListService) { }
 
   ngOnInit(): void {
   }
   
-  onIngAdded(name: any,amount: any){
-    const newIng = new Ingridient(name.value,amount.value)
+  onIngAdded(form : NgForm){
+    let value  = form.value
+    const newIng = new Ingridient(value.name,value.amount)
     this._shopSrv.addIngridient(newIng)
   }
 
